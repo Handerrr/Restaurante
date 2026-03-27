76dd02f9333d8bd44ecdd934feffb857e4ef2a25
Handerrr <hunterxxx2610@gmail.com>
2026-03-26 22:45:55 -0300

    third-commit

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -2,31 +2,32 @@ import Hero from '../../Components/Hero'
 import RestaurantList from '../../Components/RestaurantList'
 import type Restaurant from '../../models/Restaurant'
 import trattoria from '../../assets/Trattoria.png'
+import sushi from '../../assets/sushi.png'
 
 const pratos: Restaurant[] = [
   {
-    title: 'La Dolce Vita Trattoria',
+    title: 'Hioki Sushi',
     rating: 4.9,
     category: '',
     description:
-      'A La Dolce Vita Trattoria leva a autêntica cozinha italiana até você! Desfrute de massas caseiras, pizzas deliciosas e risotos incríveis, tudo no conforto do seu lar. Entrega rápida, pratos bem embalados e sabor inesquecível. Peça já!',
-    image: trattoria,
-    infos: ['Destaque da semana', 'Italiana'],
+      'Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida.Experimente o Japão sem sair do lar com nosso delivery!',
+    image: sushi,
+    infos: ['Japonesa'],
     id: 1,
   },
   {
     title: 'La Dolce Vita Trattoria',
-    rating: 4.9,
+    rating: 4.6,
     category: '',
     description:
       'A La Dolce Vita Trattoria leva a autêntica cozinha italiana até você! Desfrute de massas caseiras, pizzas deliciosas e risotos incríveis, tudo no conforto do seu lar. Entrega rápida, pratos bem embalados e sabor inesquecível. Peça já!',
     image: trattoria,
-    infos: ['Destaque da semana', 'Italiana'],
+    infos: ['Italiana'],
     id: 2,
   },
   {
     title: 'La Dolce Vita Trattoria',
-    rating: 4.9,
+    rating: 4.6,
     category: '',
     description:
       'A La Dolce Vita Trattoria leva a autêntica cozinha italiana até você! Desfrute de massas caseiras, pizzas deliciosas e risotos incríveis, tudo no conforto do seu lar. Entrega rápida, pratos bem embalados e sabor inesquecível. Peça já!',
@@ -36,32 +37,32 @@ const pratos: Restaurant[] = [
   },
   {
     title: 'La Dolce Vita Trattoria',
-    rating: 4.9,
-    category: 'Italiana',
+    rating: 4.6,
+    category: '',
     description:
       'A La Dolce Vita Trattoria leva a autêntica cozinha italiana até você! Desfrute de massas caseiras, pizzas deliciosas e risotos incríveis, tudo no conforto do seu lar. Entrega rápida, pratos bem embalados e sabor inesquecível. Peça já!',
     image: trattoria,
-    infos: ['Destaque da semana', 'Italiana'],
+    infos: ['Italiana'],
     id: 4,
   },
   {
     title: 'La Dolce Vita Trattoria',
-    rating: 4.9,
+    rating: 4,
     category: 'Italiana',
     description:
       'A La Dolce Vita Trattoria leva a autêntica cozinha italiana até você! Desfrute de massas caseiras, pizzas deliciosas e risotos incríveis, tudo no conforto do seu lar. Entrega rápida, pratos bem embalados e sabor inesquecível. Peça já!',
     image: trattoria,
-    infos: ['Destaque da semana', 'Italiana'],
+    infos: ['Italiana'],
     id: 5,
   },
   {
     title: 'La Dolce Vita Trattoria',
-    rating: 4.9,
+    rating: 4.6,
     category: 'Italiana',
     description:
       'A La Dolce Vita Trattoria leva a autêntica cozinha italiana até você! Desfrute de massas caseiras, pizzas deliciosas e risotos incríveis, tudo no conforto do seu lar. Entrega rápida, pratos bem embalados e sabor inesquecível. Peça já!',
     image: trattoria,
-    infos: ['Destaque da semana', 'Italiana'],
+    infos: ['Italiana'],
     id: 6,
   },
 ]
